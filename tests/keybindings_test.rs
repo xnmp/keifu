a661512804031ab4bfe101ee17fe201498334a16
Harness - gpt-5.6-luna <chonw89@gmail.com>
2026-08-01 15:10:37 +1000

    green: isolate l pull regression test

--- tests/keybindings_test.rs
+++ tests/keybindings_test.rs
@@ -247,15 +247,7 @@ fn graph_mode_branch_navigation() {
         Some(Action::PrevBranch)
     );
     assert_eq!(map_normal_graph(key(KeyCode::Char('h'))), None);
-    assert_eq!(map_normal_graph(key(KeyCode::Char('l'))), Some(Action::Pull));
-}
-
-#[test]
-fn graph_mode_l_pulls() {
-    assert_eq!(
-        map_normal_graph(key(KeyCode::Char('l'))),
-        Some(Action::Pull)
-    );
+    assert_eq!(map_normal_graph(key(KeyCode::Char('l'))), None);
 }
 
 #[test]
