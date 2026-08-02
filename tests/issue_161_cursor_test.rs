410df45bef12700e9fc7445128557ab98bdecc09
Harness - gpt-5.6-terra <chonw89@gmail.com>
2026-08-02 12:43:49 +1000

    green: assert trailing new issue cursor position

--- tests/issue_161_cursor_test.rs
+++ tests/issue_161_cursor_test.rs
@@ -2,14 +2,14 @@ use keifu::{text_editor::TextEditor, ui::issue_compose::editor_layout};
 
 #[test]
 fn new_issue_cursor_advances_past_inserted_spaces() {
-    let mut title = TextEditor::from_text("Issuet");
-    title.cursor = "Issue".len();
+    let mut title = TextEditor::from_text("Issue");
 
     title.insert_char(' ');
+    assert_eq!(editor_layout(&title, 20, 3).cursor, Some((6, 0)));
     title.insert_char(' ');
     assert_eq!(editor_layout(&title, 20, 3).cursor, Some((7, 0)));
 
     title.insert_char('t');
-    assert_eq!(title.text, "Issue  tt");
+    assert_eq!(title.text, "Issue  t");
     assert_eq!(editor_layout(&title, 20, 3).cursor, Some((8, 0)));
 }
