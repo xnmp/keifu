76333f76562fa0cbd445cb202800f2a304703e05
Harness - gpt-5.6-sol <chonw89@gmail.com>
2026-08-02 18:24:11 +1000

    plan: expose browser PR action seams

--- src/app/commit_menu_actions.rs
+++ src/app/commit_menu_actions.rs
@@ -672,6 +672,7 @@ impl App {
             CommitMenuItem::CreatePr => {
                 self.open_create_pr();
             }
+            CommitMenuItem::OpenPrInBrowser => {}
             CommitMenuItem::MergePr => {
                 self.open_merge_pr();
             }
